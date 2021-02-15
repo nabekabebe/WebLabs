54a4271f596104fb48666f13d79f18a67a374af6
nabek <nabekabebe6@gmail.com>
2021-02-15 10:58:51 +0300

    lab 7 added. api fetch with search functionality.

---
+++ Lab_Ex_07/Finished/asset/js/loadPostsAPI.js
@@ -0,0 +1,122 @@
+const search = document.querySelector('#search');
+
+// UI Vars
+const postDiv3 = document.getElementById('thePosts');
+search.addEventListener('keyup', filterTasks);
+//Load Every thing ....
+document.addEventListener('DOMContentLoaded', () => {
+  //load_fromPlaceHolder();
+  loadDataNew();
+});
+
+//load a single customer function
+function load_fromPlaceHolder() {
+  //open the request
+  fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(function (res) {
+      return res.json(); //return the JSON Promise
+    })
+    .then(function (posts) {
+      //iterate over each post [100 posts]
+      let output = '';
+      posts.forEach(function (post) {
+        output += `
+        
+                <div class="item">
+                <div class="image">
+                    <img src=" https://images.unsplash.com/photo-1499482125586-91609c0b5fd4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80">
+                </div>
+                <div class="content">
+                    <a class="header" href="#" id="bTitle">
+                    ${post.title.toUpperCase()}
+                    </a>
+                    <div class="description">
+                        <p id="bDesc">
+                        ${post.body}
+                        </p>
+                    </div>
+                    <div class="extra">
+                        <a class="ui floated basic violet button" href="#">Read Mores</a>
+                    </div>
+                </div>
+            </div>
+        
+        `;
+      });
+      postDiv3.innerHTML = output;
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+async function load_fromPlaceHolder_new() {
+  //open the request
+  let response = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+  let data = await response.json();
+
+  return data;
+}
+
+function loadDataNew() {
+  load_fromPlaceHolder_new()
+    .then(function (posts) {
+      //iterate over each post [100 posts]
+      let output = '';
+      posts.forEach(function (post) {
+        output += `
+
+    <div class="item">
+        <div class="image">
+            <img src=" https://images.unsplash.com/photo-1499482125586-91609c0b5fd4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80">
+        </div>
+        <div class="content">
+            <a class="header" href="#" id="bTitle">
+            ${post.title.toUpperCase()}
+            </a>
+            <div class="description">
+                <p id="bDesc">
+                ${post.body}
+                </p>
+            </div>
+            <div class="extra">
+                <a class="ui floated basic violet button" href="#">Read Mores</a>
+            </div>
+        </div>
+    </div>
+
+`;
+      });
+      postDiv3.innerHTML = output;
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+function filterTasks(e) {
+  let posts = document.querySelectorAll('#thePosts .item #bTitle');
+  posts.forEach((el) => {
+    if (el.textContent.toLowerCase().includes(e.target.value.toLowerCase())) {
+      el.parentElement.parentElement.style.display = 'flex';
+    } else {
+      el.parentElement.parentElement.style.display = 'none';
+    }
+  });
+}
+// function sortIt(direction) {
+
+//     var tasks = Array.from(document.querySelectorAll('#thePosts .item #bTitle')).sort(function (a, b) {
+//       a = a.lastChild.innerHTML;
+//       b = b.lastChild.innerHTML;
+//       if (Number(direction) == 1) {
+//         return a > b ? 1 : a < b ? -1 : 0;
+//       } else {
+//         return a < b ? 1 : a > b ? -1 : 0;
+//       }
+//     });
+
+//     clearAllTask();
+//     taskList.append(...tasks);
+//   }
